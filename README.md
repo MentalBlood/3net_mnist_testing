b85d0712e6d91b7e2a23614045d477c89e1871f2
MentalBlood <neceporenkostepan@gmail.com>
2020-11-25 13:40:38 +0300

    added tests results

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Containerizable service for testing service represented in "3net_mnist"
 
 ## Build
-    docker build -t new_image .
+    docker build -t new_image_testing .
 
 ## Run
-    docker run -it --rm -d -p 8080:80 --name 3net_mnist_testing new_image
+    docker run -it --rm -d -p 8090:80 --name 3net_mnist_testing new_image_testing
--- tester.py
+++ tester.py
@@ -1,13 +1,21 @@
 import requests
 import json
 
-testUrl = 'http://localhost:8080/test'
+testUrl = 'http://localhost:8090/test'
 headers = {'content-type': 'application/json'}
 
 url = 'http://172.17.0.2:80/'
-tests = ['v1_module_fashion-mnist']
+# tests = [
+# 	'v1_module_mnist',
+# 	'v1_module_fashion-mnist',
+# 	'v2_module_mnist',
+# 	'v2_module_fashion-mnist'
+# ]
+tests = [
+	'v2_integration'
+]
 
-response = requests.post(testUrl, json={'url': url}, headers=headers)
+response = requests.post(testUrl, json={'url': url, 'tests': tests}, headers=headers)
 response_json = json.loads(response.text)
 
 print(response_json['report'])
